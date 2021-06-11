e4ecca1759138688bea5e9d60a63d6bab7574562
Dorine van der Voorden <84196122+Dorinevandervoorden@users.noreply.github.com>
2021-06-11 07:07:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc Academy Studenten");
+
+/* dit is een block comment om alles uit te zetten
+// dit is een grote som
+let product = 1230941 * 1823794;
+console.log('product', product);
+
+let quotient = 637263 / 54;
+console.log('quotient', quotient);
+
+*/
